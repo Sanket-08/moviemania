8ef785fdecf0882b2d9b6fce00bf4480451c0649
Sanket Nishikant Patil <109276505+Sanket-08@users.noreply.github.com>
2023-07-18 05:40:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,13 +42,13 @@ const App = () => {
         <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
-          ))}
+          ))};
         </div>
       ) : (
         <div className="empty">
           <h2>No movies found</h2>
         </div>
-      )}
+      )};
     </div>
   );
 };
